b17b29d588a1f5fc90295e3552855e58124dacbe
JunyoungYoon <junyoungha2@naver.com>
2023-01-30 12:30:06 +0900

    .
    .

--- ShoppingMallProject2/src/menu_memeber/MemberQuit.java
+++ ShoppingMallProject2/src/menu_memeber/MemberQuit.java
@@ -2,6 +2,7 @@ package menu_memeber;
 
 
 import _mall.MenuCommand;
+import cart.CartDAO;
 import controller.MallController;
 import member.MemberDAO;
 import util.Util;
@@ -9,10 +10,12 @@ import util.Util;
 public class MemberQuit implements MenuCommand{
 	MallController mallCont;
 	MemberDAO mdao;
+	CartDAO cdao;
 	@Override
 	public void init() {
 		mallCont = MallController.getInstance();
 		mdao = MemberDAO.getInstance();
+		cdao = CartDAO.getInstance();
 	}
 
 	@Override
@@ -22,6 +25,7 @@ public class MemberQuit implements MenuCommand{
 		String pw = Util.getValue("[ 회원탈퇴 ] PW : ");
 		if (mdao.checkMember(id, pw)) {
 			mdao.deleteMember(id);
+			cdao.clearMyCart(id);
 			System.out.println("[ 회원탈퇴 ] 완료");
 		} else {
 			System.err.println("비밀번호가 일치하지 않습니다");
